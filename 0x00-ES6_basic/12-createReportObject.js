da3c2841143e7080646d4f3e3471095960b570b5
Nomee07 <gigabanomfundo@gmail.com>
2024-04-22 14:11:20 +0000

    Arrow Functions

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const getNumberOfDepartments = () => Object.keys(employeesList).length;
+
+  const reportObject = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments,
+  };
+
+  return reportObject;
+}
